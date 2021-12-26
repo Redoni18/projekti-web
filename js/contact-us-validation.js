c77b90f111c65135f58dc99705e2cb695f79242d
vullnetk <vk53084@ubt-uni.net>
2021-12-26 18:04:24 +0100

    added technology, apparel, contact-us-validation,

---
+++ js/contact-us-validation.js
@@ -0,0 +1,30 @@
+function validoMesazhin(){
+    if(document.getElementById('emri').value == null || document.getElementById('emri').value === ''){
+        alert('Duhet të plotësoni emrin!');
+        return;
+    }
+    else if(document.getElementById('email').value == null || document.getElementById('email').value === ''){
+        alert('Duhet të plotësoni emailin!');
+        return;
+    }
+    else if(!validoRegexE()){
+        alert('Emaili nuk është plotësuar si duhet!')
+        return;
+    }else if(document.getElementById('mesazhi').value ==null || document.getElementById('mesazhi').value === ''){
+        alert('Shkruani mesazhin tuaj!');
+        return;
+    }
+    else {
+        alert('Mesazhi është dërguar!')
+        document.getElementById('emri').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('mesazhi').value = '';
+    }
+}
+function validoRegexE(){
+    var regex = /^([a-zA-Z\d\.\-\_]+)@([a-zA-Z0-9]+).([a-z]{2,10})$/;
+    var email = document.getElementById('email').value;
+    regex.test(email);
+    var validimi = regex.test(email);
+    return validimi;
+}
